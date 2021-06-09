3c73414565909db4d25ae5636cddcc4cfc92f0f5
Celal Ergün <celalergun@gmail.com>
2021-06-09 16:54:19 +0300

    Stok liste formuna seçim eklendi

--- StokListeForm.Designer.cs
+++ StokListeForm.Designer.cs
@@ -29,13 +29,13 @@ namespace VTSOdevStokCari
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtArama = new System.Windows.Forms.TextBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,22 +53,6 @@ namespace VTSOdevStokCari
             this.dataGridView1.Size = new System.Drawing.Size(459, 711);
             this.dataGridView1.TabIndex = 6;
             // 
-            // Column1
-            // 
-            this.Column1.HeaderText = "Stok Adı";
-            this.Column1.Name = "Column1";
-            this.Column1.ReadOnly = true;
-            this.Column1.Width = 200;
-            // 
-            // Column2
-            // 
-            dataGridViewCellStyle1.Format = "N2";
-            dataGridViewCellStyle1.NullValue = null;
-            this.Column2.DefaultCellStyle = dataGridViewCellStyle1;
-            this.Column2.HeaderText = "Mevcut";
-            this.Column2.Name = "Column2";
-            this.Column2.ReadOnly = true;
-            // 
             // button2
             // 
             this.button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
@@ -81,32 +65,53 @@ namespace VTSOdevStokCari
             // 
             // button1
             // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "Tamam";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // txtArama
             // 
-            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.txtArama.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBox1.Location = new System.Drawing.Point(250, 14);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(221, 20);
-            this.textBox1.TabIndex = 7;
+            this.txtArama.Location = new System.Drawing.Point(250, 14);
+            this.txtArama.Name = "txtArama";
+            this.txtArama.Size = new System.Drawing.Size(221, 20);
+            this.txtArama.TabIndex = 7;
+            this.txtArama.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "StokAdi";
+            this.Column1.HeaderText = "Stok Adı";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "Miktar";
+            dataGridViewCellStyle2.Format = "N2";
+            dataGridViewCellStyle2.NullValue = null;
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2.HeaderText = "Mevcut";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
             // 
             // StokListeForm
             // 
+            this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.button2;
             this.ClientSize = new System.Drawing.Size(483, 764);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtArama);
             this.Name = "StokListeForm";
             this.Text = "Stok Listesi";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
@@ -118,10 +123,10 @@ namespace VTSOdevStokCari
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtArama;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
     }
 }
--- StokListeForm.cs
+++ StokListeForm.cs
@@ -12,23 +12,45 @@ namespace VTSOdevStokCari
 {
     public partial class StokListeForm : Form
     {
-        private List<Musteri> _musteriler = null;
+        private List<Stok> _stoklar = null;
+        BindingSource liste = null;
+
+        public Stok Secilen { get; set; }
 
         public Veritabani VeriTabani { get; internal set; }
 
         public StokListeForm()
         {
             InitializeComponent();
+            Secilen = new Stok();
+            Secilen.StokID = -1;
         }
 
-        private void ListeyiYukle()
+        internal void Yukle()
         {
-            _musteriler = VeriTabani.Musteriler("");
+            _stoklar = VeriTabani.Stoklar("");
+            dataGridView1.Refresh();
+            liste = new BindingSource();
+            liste.DataSource = _stoklar;
+            liste.CurrentChanged += Liste_CurrentChanged;
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = liste;
+            Liste_CurrentChanged(null, null);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Liste_CurrentChanged(object sender, EventArgs e)
         {
+            if (liste.Current == null)
+                return;
 
+            Text = ((Stok)liste.Current).StokAdi + " seçildi";
+            Secilen = ((Stok)liste.Current);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            _stoklar = VeriTabani.Stoklar(txtArama.Text);
+            dataGridView1.Refresh();
         }
     }
 }
