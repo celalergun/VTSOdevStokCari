f6a6e63a4c32a8fc3fc8d15831764f287f2d3bf3
Celal Ergün <celalergun@gmail.com>
2021-06-09 16:53:55 +0300

    Stok kartı girişi eklendi

---
+++ StokKartForm.Designer.cs
@@ -0,0 +1,199 @@
+﻿
+namespace VTSOdevStokCari
+{
+    partial class StokKartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtMiktar = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBirim = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtStokAdi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(592, 17);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Kaydet";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(511, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(430, 17);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 17;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(402, 426);
+            this.dataGridView1.TabIndex = 16;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "StokAdi";
+            this.Column1.HeaderText = "Stok Adı";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 200;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "Miktar";
+            dataGridViewCellStyle2.Format = "N2";
+            dataGridViewCellStyle2.NullValue = null;
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle2;
+            this.Column2.HeaderText = "Miktar";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // txtMiktar
+            // 
+            this.txtMiktar.BackColor = System.Drawing.SystemColors.Info;
+            this.txtMiktar.Location = new System.Drawing.Point(484, 119);
+            this.txtMiktar.Name = "txtMiktar";
+            this.txtMiktar.ReadOnly = true;
+            this.txtMiktar.Size = new System.Drawing.Size(102, 20);
+            this.txtMiktar.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(427, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Miktar";
+            // 
+            // txtBirim
+            // 
+            this.txtBirim.Location = new System.Drawing.Point(484, 93);
+            this.txtBirim.Name = "txtBirim";
+            this.txtBirim.Size = new System.Drawing.Size(102, 20);
+            this.txtBirim.TabIndex = 13;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(427, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Birim";
+            // 
+            // txtStokAdi
+            // 
+            this.txtStokAdi.Location = new System.Drawing.Point(484, 67);
+            this.txtStokAdi.Name = "txtStokAdi";
+            this.txtStokAdi.Size = new System.Drawing.Size(234, 20);
+            this.txtStokAdi.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(427, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Stok adı";
+            // 
+            // StokKartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtMiktar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBirim);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtStokAdi);
+            this.Controls.Add(this.label1);
+            this.Name = "StokKartForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Stok Kartları";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtMiktar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBirim;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtStokAdi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
---
+++ StokKartForm.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VTSOdevStokCari
+{
+    public partial class StokKartForm : Form
+    {
+        private List<Stok> _stoklar = null;
+        BindingSource liste = null;
+        public Veritabani VeriTabani { get; internal set; }
+        public StokKartForm()
+        {
+            InitializeComponent();
+        }
+
+        internal void Yukle()
+        {
+            _stoklar = VeriTabani.Stoklar("");
+            dataGridView1.Refresh();
+            liste = new BindingSource();
+            liste.DataSource = _stoklar;
+            liste.CurrentChanged += Liste_CurrentChanged;
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = liste;
+            Liste_CurrentChanged(null, null);
+        }
+
+        private void Liste_CurrentChanged(object sender, EventArgs e)
+        {
+            if (liste.Current == null)
+                return;
+
+            txtStokAdi.Text = ((Stok)liste.Current).StokAdi;
+            txtBirim.Text = ((Stok)liste.Current).Birim;
+            txtMiktar.Text = ((Stok)liste.Current).Miktar.ToString("N2");
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            string eklemeKodu = "INSERT INTO Stok (StokAdi, Birim, Miktar) VALUES (@StokAdi, @Birim, 0)";
+            SqliteCommand komut = new SqliteCommand(eklemeKodu, VeriTabani.Baglanti);
+            komut.Parameters.AddWithValue("@StokAdi", txtStokAdi.Text);
+            komut.Parameters.AddWithValue("@Birim", txtBirim.Text);
+            komut.Prepare();
+            komut.ExecuteNonQuery();
+            Yukle();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (liste.Current == null)
+                return;
+
+            string adi = ((Stok)liste.Current).StokAdi;
+            DialogResult sonuc = MessageBox.Show($"{adi} kaydı silinecek, emin misiniz?", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (sonuc != DialogResult.Yes)
+                return;
+
+            int id = ((Stok)liste.Current).StokID;
+            string silmeKodu = "DELETE FROM Stok WHERE StokID = @StokID";
+            SqliteCommand komut = new SqliteCommand(silmeKodu, VeriTabani.Baglanti);
+            komut.Parameters.AddWithValue("@StokID", id);
+            komut.Prepare();
+            komut.ExecuteNonQuery();
+            Yukle();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (liste.Current == null)
+                return;
+
+            int id = ((Stok)liste.Current).StokID;
+            string guncellemeKodu = "UPDATE Stok SET StokAdi = @MustesiUnvani, Birim = @Birim WHERE StokID = @StokID";
+            SqliteCommand komut = new SqliteCommand(guncellemeKodu, VeriTabani.Baglanti);
+            komut.Parameters.AddWithValue("@StokID", id);
+            komut.Parameters.AddWithValue("@StokAdi", txtStokAdi.Text);
+            komut.Parameters.AddWithValue("@Birim", txtBirim.Text);
+            komut.Prepare();
+            komut.ExecuteNonQuery();
+            Yukle();
+        }
+    }
+}
