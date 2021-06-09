0e845e53b233d54aff8cf992ae624cfa502c1310
Celal Ergün <celalergun@gmail.com>
2021-06-09 18:21:54 +0300

    Cari hareket giriş formu tasarlandı ve gerçekleştirildi

---
+++ CariHareketForm.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace VTSOdevStokCari
+{
+    partial class CariHareketForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUnvan = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(425, 233);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Kaydet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Alacak",
+            "Borç"});
+            this.comboBox1.Location = new System.Drawing.Point(379, 206);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(380, 180);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(301, 209);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Tip seçiniz";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(301, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Miktar giriniz";
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.BackColor = System.Drawing.SystemColors.Info;
+            this.txtAdres.Location = new System.Drawing.Point(111, 54);
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.Size = new System.Drawing.Size(102, 20);
+            this.txtAdres.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(425, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Gözat";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Info;
+            this.label1.Location = new System.Drawing.Point(28, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Müşteri seçiniz";
+            // 
+            // txtUnvan
+            // 
+            this.txtUnvan.BackColor = System.Drawing.SystemColors.Info;
+            this.txtUnvan.Location = new System.Drawing.Point(111, 28);
+            this.txtUnvan.Name = "txtUnvan";
+            this.txtUnvan.Size = new System.Drawing.Size(308, 20);
+            this.txtUnvan.TabIndex = 9;
+            // 
+            // CariHareketForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtUnvan);
+            this.Name = "CariHareketForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Cari Hareket Giriş";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUnvan;
+    }
+}
---
+++ CariHareketForm.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VTSOdevStokCari
+{
+    public partial class CariHareketForm : Form
+    {
+        public Veritabani VeriTabani { get; internal set; }
+
+        private Musteri _secilen = null;
+
+        public CariHareketForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (MusteriListeForm ml = new MusteriListeForm())
+            {
+                ml.VeriTabani = VeriTabani;
+                ml.Yukle();
+                if (ml.ShowDialog() == DialogResult.OK)
+                {
+                    txtUnvan.Text = ml.Secilen.MusteriUnvani;
+                    txtAdres.Text = ml.Secilen.Adres;
+                    _secilen = ml.Secilen;
+                    comboBox1.SelectedIndex = 0;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (_secilen == null)
+            {
+                MessageBox.Show("Önce bir müşteri kartı seçmelisiniz", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int tipi;
+            if (comboBox1.SelectedIndex == 0)
+                tipi = 1;
+            else
+                tipi = -1;
+
+            decimal miktar = numericUpDown1.Value;
+            DateTime tarih = DateTime.Now;
+
+            string eklemeKodu = "INSERT INTO CariHareket (MusteriID, Meblag, Tipi, Tarih) VALUES (@MusteriID, @Meblag, @Tipi, @Tarih)";
+            SqliteCommand komut = new SqliteCommand(eklemeKodu, VeriTabani.Baglanti);
+            komut.Parameters.AddWithValue("@MusteriID", _secilen.MusteriID);
+            komut.Parameters.AddWithValue("@Meblag", miktar);
+            komut.Parameters.AddWithValue("@Tipi", tipi);
+            komut.Parameters.AddWithValue("@Tarih", tarih);
+            komut.Prepare();
+            komut.ExecuteNonQuery();
+            Close();
+        }
+    }
+}
