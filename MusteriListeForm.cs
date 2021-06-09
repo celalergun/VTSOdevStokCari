8af72dbf07b63b1b1a57b7dc748bc1268387e993
Celal Ergün <celalergun@gmail.com>
2021-06-09 06:19:36 +0300

    Müşteri listesine arama fonksiyonu eklendi

--- MusteriListeForm.cs
+++ MusteriListeForm.cs
@@ -12,24 +12,28 @@ namespace VTSOdevStokCari
 {
     public partial class MusteriListe : Form
     {
-        private List<Musteri> _musteriler = new List<Musteri>();
+        private List<Musteri> _musteriler = null;
         
         public MusteriListe()
         {
             InitializeComponent();
             ListeyiYukle();
+            BindingSource liste = new BindingSource();
+            liste.DataSource = _musteriler;
+
+            dataGridView1.DataSource = liste;
         }
 
         public Veritabani VeriTabani { get; internal set; }
 
         private void ListeyiYukle()
         {
-            
+            _musteriler = VeriTabani.Musteriler("");
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
+            _musteriler = VeriTabani.Musteriler(textBox1.Text);
         }
     }
 }
